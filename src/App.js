73b065b61d6d983d9ea5a52ec98975a5e4d24b15
danuwarisman <danuwarisman514@gmail.com>
2026-02-14 21:16:44 +0700

    Replace CartPage with CartItem in routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { store } from './store';
 import LandingPage from './components/LandingPage';
 import AboutUs from './components/AboutUs';
 import ProductList from './components/ProductList';
-import CartPage from './components/CartPage'; // <--- UPDATED IMPORT
+import CartItem from './components/CartItem'; 
 import './App.css';
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/cart" element={<CartPage />} /> {/* <--- UPDATED COMPONENT */}
+          <Route path="/cart" element={<CartItem />} />
         </Routes>
       </Router>
     </Provider>
